f2de55df3a570bac94bed5b30cb324bf41611c49
Actulurus <actulurus@gmail.com>
2024-04-03 19:26:29 +0200

    wait test

--- modules/testobjects.lua
+++ modules/testobjects.lua
@@ -1,28 +1,11 @@
-function addObjects()
-    for i = 1, 10 do
-        table.insert(game.Paths.TestObjects, "Object "..i)
-
-        coroutine.yield()
-    end
-end
-
 return {init = function(_game)
     game = _game
 
-    game.Paths.TestObjects = {"Object 1"}
-    
-    local co = coroutine.create(addObjects)
+    game.Paths.TestObjects = {}
 
-    -- game.RunService:Connect("Heartbeat", function()
-    --     coroutine.resume(co)
-    -- end)
-
-    local fnc
-    fnc = function()
-        coroutine.resume(co)
-
-        game.RunService:Delay(1, fnc)
+    for i = 1, 10 do
+        table.insert(game.Paths.TestObjects, "Object "..i)
+        
+        game.RunService:Wait(1)
     end
-
-    game.RunService:Delay(1, fnc)
 end}
