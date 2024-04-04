e0fba424f3075dd21bb07827bf0991b92bb70acc
Actulurus <actulurus@gmail.com>
2024-04-04 10:11:50 +0200

    Add objects and functions to game module

--- modules/test.lua
+++ modules/test.lua
@@ -1,11 +1,20 @@
 return {init = function(game)
-    game.RunService:Connect("RenderStepped", function(dt)
-        print("RenderStepped", dt)
+    game.ObjectService:Add({
+        X = 0,
+        Y = 0,
+        Width = 150,
+        Height = 150,
+        ZIndex = 9,
+        Color = {1, 0, 1, 1}
+    })
 
-        love.graphics.print("RenderStepped, delta: "..tostring(dt), 10, 10)
-
-        for i, v in ipairs(game.Paths.TestObjects) do
-            love.graphics.print(v, 10, 10 + i * 20)
+    local x, y = 0, 0
+    game.ObjectService:Add({
+        ZIndex = 10,
+        Function = function(dt)
+            x = x + dt * 40
+            love.graphics.setColor(1, 1, 0.5, 1)
+            love.graphics.rectangle("fill", x, y, 50, 50)
         end
-    end)
+    })
 end}
