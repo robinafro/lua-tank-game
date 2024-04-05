77570dd902061a37b8d14726ef0b7818d0c21cd4
Actulurus <actulurus@gmail.com>
2024-04-05 22:21:38 +0200

    Update camera behavior and settings

--- classes/camera.lua
+++ classes/camera.lua
@@ -4,13 +4,16 @@ Camera.__index = Camera
 function Camera.new()
     local self = setmetatable({}, Camera)
 
-    self.RenderDistanceScreenMultiplier = 1.5
+    self.RenderDistanceScreenMultiplier = 1
 
     self.X = 0
     self.Y = 0
     self.RenderDistance = 0
-    self.MaxTweenSpeed = 2
+    self.MaxTweenSpeed = 1
+
     self.Target = nil
+    self.TargetPosition = {X = 0, Y = 0}
+    self.TargetZoneRadiusPercentage = 0.0
 
     return self
 end
@@ -32,11 +35,19 @@ function Camera:Render(renderables, dt)
     self.RenderDistance = math.max(love.graphics.getWidth(), love.graphics.getHeight()) * self.RenderDistanceScreenMultiplier
 
     if self.Target then
-        self:MoveTo(self.Target.X, self.Target.Y)
+        local distance = math.sqrt(math.abs((self.Target.X + self.Target.Width / 2) - (self.X + love.graphics.getWidth() / 2)) ^ 2 + math.abs((self.Target.Y + self.Target.Height / 2) - (self.Y + love.graphics.getHeight() / 2)) ^ 2)
+        local maxDistance = self.TargetZoneRadiusPercentage / 2 * math.max(love.graphics.getWidth(), love.graphics.getHeight())
+
+        if distance > maxDistance then
+            self.TargetPosition.X = self.Target.X
+            self.TargetPosition.Y = self.Target.Y
+        end
+
+        self:MoveTo(self.TargetPosition.X, self.TargetPosition.Y)
     end
 
     for _, renderable in ipairs(renderables) do
-        local distanceFromCenter = math.sqrt(math.abs(renderable.X - (self.X + love.graphics.getWidth() / 2)) ^ 2 + math.abs(renderable.Y - (self.Y + love.graphics.getHeight() / 2)) ^ 2)
+        local distanceFromCenter = math.sqrt(math.abs((renderable.X + renderable.Width / 2) - (self.X + love.graphics.getWidth() / 2)) ^ 2 + math.abs((renderable.Y + renderable.Height / 2) - (self.Y + love.graphics.getHeight() / 2)) ^ 2)
 
         if distanceFromCenter < self.RenderDistance or renderable.AlwaysVisible == true then
             love.graphics.setColor(unpack(renderable.Color or {1, 1, 1, 1}))
