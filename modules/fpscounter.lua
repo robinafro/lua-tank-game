89be0f32fd1e95e9f17a3d6933d936fd21f1185e
Actulurus <actulurus@gmail.com>
2024-04-06 21:04:36 +0200

    Update FPS counter to display accurate frames per second

--- modules/fpscounter.lua
+++ modules/fpscounter.lua
@@ -1,6 +1,6 @@
 return {init = function(game)
     obj = require("classes.object").new(function(dt)
-        love.graphics.print("FPS: " .. love.timer.getFPS(), 7, love.graphics.getHeight() - 23)
+        love.graphics.print("FPS: " .. math.floor(1 / dt), 7, love.graphics.getHeight() - 23)
     end)
     
     obj.ZIndex = 1000
@@ -8,8 +8,4 @@ return {init = function(game)
     obj.HUD = true
 
     game.ObjectService:Add(obj)
-
-    -- game.RunService:Connect("RenderStepped", function(dt)
-    --     print(1 / dt)
-    -- end)
 end}
