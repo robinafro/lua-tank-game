92cc8ffcdfd3b3d0bc903c1e8124be28de36c150
Actulurus <actulurus@gmail.com>
2024-04-07 20:36:25 +0200

    Update NeuralNetwork input size and add distance calculation

--- classes/enemy.lua
+++ classes/enemy.lua
@@ -7,7 +7,7 @@ Enemy.__index = Enemy
 function Enemy.new()
     local self = setmetatable({}, Enemy)
 
-    self.NeuralNetwork = NeuralNetwork.new(4, 4, 3, 0.1)
+    self.NeuralNetwork = NeuralNetwork.new(5, 4, 3, 0.1)
     self.Target = nil
 
     self.NeuralNetwork:printWeights()
@@ -30,11 +30,14 @@ function Enemy:Update(dt)
         Y = vectorToTarget.Y * math.cos(self.Controlling.Rotation) - vectorToTarget.X * math.sin(self.Controlling.Rotation)
     }
 
+    local distance = math.sqrt(vectorToTarget.X ^ 2 + vectorToTarget.Y ^ 2)
+
     local angleDifference = self.Controlling.Rotation - self.Target.Rotation
 
     local inputs = {
         vectorToTargetRelative.X,
         vectorToTargetRelative.Y,
+        distance,
         angleDifference,
         self.Target.ForwVelocity
     }
@@ -47,7 +50,7 @@ function Enemy:Update(dt)
 
     print(forward)
 
-    self.Controlling:Move(forward, angular, true)
+    self.Controlling:Move(forward, angular)
     self.Controlling:Update(dt)
 
     if shoot > 0.5 then
