70d85248345eb8166547c1a7042edff4315d373c
Actulurus <actulurus@gmail.com>
2024-04-17 15:04:44 +0200

    Collapsed if statements into one condition. Increased performance by a tiny bit

--- classes/collider.lua
+++ classes/collider.lua
@@ -174,29 +174,25 @@ end
 
 function Collider:Collide(colliders, ignoreList)
     for i, collider in pairs(colliders) do
-        if not (ignoreList or {})[collider.ID] and collider.Object and self.Object then
-            if collider ~= self then
-                if self:Collides(collider) then
-                    local min_penetration_axis, overlap = self:CheckCollisionSAT(collider) 
-                    if min_penetration_axis then
-                        if self.CanTouch and collider.CanTouch then
-                            if self.CollisionFunction then
-                                self.CollisionFunction(self, collider)
-                            end
-
-                            if collider.CollisionFunction then
-                                collider.CollisionFunction(collider, self)
-                            end
-                        end
-
-                        if self.CanCollide and collider.CanCollide then
-                            self:ComputeCollision(collider, min_penetration_axis, overlap)
-
-                            min_penetration_axis, overlap = collider:CheckCollisionSAT(self) 
-
-                            collider:ComputeCollision(self, min_penetration_axis, overlap)
-                        end
+        if not (ignoreList or {})[collider.ID] and collider.Object and self.Object and collider ~= self and self:Collides(collider) then
+            local min_penetration_axis, overlap = self:CheckCollisionSAT(collider) 
+            if min_penetration_axis then
+                if self.CanTouch and collider.CanTouch then
+                    if self.CollisionFunction then
+                        self.CollisionFunction(self, collider)
                     end
+
+                    if collider.CollisionFunction then
+                        collider.CollisionFunction(collider, self)
+                    end
+                end
+
+                if self.CanCollide and collider.CanCollide then
+                    self:ComputeCollision(collider, min_penetration_axis, overlap)
+
+                    min_penetration_axis, overlap = collider:CheckCollisionSAT(self) 
+
+                    collider:ComputeCollision(self, min_penetration_axis, overlap)
                 end
             end
         end
