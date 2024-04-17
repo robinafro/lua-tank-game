458fd7a24f179c7e9e93290b1d6c768778c059d9
Actulurus <actulurus@gmail.com>
2024-04-17 15:05:10 +0200

    Grouped together colliders by proximity. The MaxDistance can be tweaked later. Increased by about 100 FPS

--- modules/colliders.lua
+++ modules/colliders.lua
@@ -1,14 +1,48 @@
+local Groups = {}
+local MaxDistance = 1000
+
+function RefreshGroups(colliders)
+    Groups = {}
+
+    for _, collider in pairs(colliders) do
+        local success = false
+        for _, group in pairs(Groups) do
+            local x, y = group[1].Object.X, group[1].Object.Y
+            local distance = math.sqrt((collider.Object.X - x) ^ 2 + (collider.Object.Y - y) ^ 2)
+
+            if distance < MaxDistance then
+                success = true
+                table.insert(group, collider)
+                break
+            end
+        end
+
+        if not success then
+            table.insert(Groups, {collider})
+        end
+    end
+end
+
 return {init = function(game)
     if not game.Paths.Colliders then
         game.Paths.Colliders = {}
     end
 
-    game.RunService:Connect("Stepped", function(dt)
-        local ignoreList = {}
+    if not game.Paths.Map then
+        repeat game.RunService:Wait() until game.Paths.Map
+    end
 
-        for _, collider in pairs(game.Paths.Colliders) do
-            collider:Collide(game.Paths.Colliders, ignoreList)
-            ignoreList[collider.ID] = true
+    local colliders = {}
+
+    game.RunService:Connect("Stepped", function(dt)
+        RefreshGroups(game.Paths.Colliders)
+
+        local ignoreList = {}
+        for _, group in pairs(Groups) do
+            for _, collider in pairs(group) do
+                collider:Collide(group, ignoreList)
+                ignoreList[collider.ID] = true
+            end
         end
     end)
 end}
